812c5c578290c77b5dfe8a67c253b7dfc16147be
BRIGHT <BRIGHT@DESKTOP-ST51CUU>
2024-02-01 21:34:16 +0300

    linq ile ilgili örnekler çoğaltıldı

--- LinqToObject/Program.cs
+++ LinqToObject/Program.cs
@@ -74,8 +74,42 @@
 
 
 
+            // en az 10 adet eleman olacak şekilde bir koleksiyon tanımlayın
+            // koleksiyonun içerisinde Personel tipi olsun 
+            // personel tipinin içerisine 5 adet field yazın
+
+            // fiedler Ad,Soyad,Maas,Departman
 
 
+            // sorgular
+
+            // Adı Ramazan olan personeli getir
+            // Maası 1000 TL'den fazla olan personelleri getir
+
+            // Departmanı Yazılım olan personelleri getir.
+
+            // Maası 3000'den büyük olan personellerin departman adını getir
+
+            //Take metodu vereceğiniz değer kadar elemanı size getirir
+
+            var student6 = stu.Take(2);
+
+            // Any metodu bize bool döner, (Verinin olup olmadığını kontrol etmek için kullanılır)
+
+            // aşağıdaki örnekte, Adı yaşar olan bir eleman var ise true dönecektir
+
+            bool yasarisOk = stu.Any(s => s.Name == "Atilla");
+
+
+            // OrderByDEscending metodu aldığı parametre ye göre tersten sıralama yapar
+            var student7 = stu.OrderByDescending(s => s.Age);
+
+
+            // OrderBy metodu aldığı parametreye göre düzden sıralama yapar(metinsel ifadelerde alfabetik sıralama yapar)
+           var student8= stu.OrderBy(s => s.Name);
+
+
+           var student9= stu.OrderBy(x=>x.Name).Skip(2);
 
         }
         static Student GetStudentByName(List<Student> stu, string name)
